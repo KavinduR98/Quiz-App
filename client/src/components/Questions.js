8e75903e4b0d7c274e06dd061387bf91672a616b
@kavinduushan07 <kavinduushan07@gmail.com>
2023-02-18 13:20:12 +0530

    create data.json file

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,25 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import data from '../database/data'
 
 export default function Questions() {
 
-    const [checked, setChecked] = useState(undefined)
+  const [checked, setChecked] = useState(undefined)
 
-    function onSelect(){
-        console.log('radio button change')
-    }
+  const question = data[0]
+
+  useEffect(() => {
+    console.log(question)
+  });
+
+  function onSelect() {
+    console.log('radio button change')
+  }
 
   return (
     <div className='questions'>
-        <h2 className='text-light'>Simple Question 1</h2>
+      <h2 className='text-light'>{question.question}</h2>
 
-        <ul>
-            <li>
-               <input type="radio" value={false} name="options" id="q1-option" onChange={onSelect()}></input> 
+      <ul key={question.id}>
+        {
+          question.options.map((q, i)=>(
+            <li key={i}>
+              <input type="radio" value={false} name="options" id={`q${i}-option`} onChange={onSelect()}></input>
 
-               <label className='text-primary' htmlFor='q1-option'>option</label>
-               <div className='check checked'></div>
+              <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
+              <div className='check'></div>
             </li>
-        </ul>
+          ))
+        }
+      </ul>
     </div>
   )
 }
---
+++ client/src/database/data.js
@@ -0,0 +1,47 @@
+export default[
+    {
+        id: 1,
+        question : "Javascript is an ........... language",
+        options : [
+            'Object-Oriented',
+            'Object-Based',
+            'Procedural',
+        ]
+    },
+    {
+        id: 2,
+        question : "What is the use of the <noscript> tag in Javascript?",
+        options : [
+            'The contents are displayed by non-JS based browsers',
+            'Clears all the cookies and cache',
+            'Both 1 and 2',
+        ]
+    },
+    {
+        id: 3,
+        question : "When the switch statement matches the expression with the given labels, how is the comparison done?",
+        options : [
+            'Both the datatype and the result of the expression are compared',
+            'Only the datatype of the expression is compared',
+            'Only the value of the expression is compared',
+        ]
+    },
+    {
+        id: 4,
+        question : "How can a datatype be declared to be a constant type?",
+        options : [
+            'const',
+            'var',
+            'let',
+        ]
+    },
+    {
+        id: 5,
+        question : "Which of the following methods is used to access HTML elements using Javascript?",
+        options : [
+            'getElementbyId()',
+            'getElementByClassName()',
+            'None of the above',
+        ]
+    },
+]
