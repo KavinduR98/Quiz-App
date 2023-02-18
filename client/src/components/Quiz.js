6b7bde0370fee7422ef66c70384400f671810103
@kavinduushan07 <kavinduushan07@gmail.com>
2023-02-18 12:46:50 +0530

    create question structure

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Questions from '../components/Questions';
 
 export default function Quiz() {
 
@@ -16,6 +17,7 @@ export default function Quiz() {
       <h1 className='title text-light'>Quiz Application</h1>
 
       {/* display questions */}
+      <Questions></Questions>
 
       <div className='grid'>
         <button className='btn prev' onClick={onPrev}>Prev</button>
