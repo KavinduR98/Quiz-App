9161ea999bb745b82a987ae20231b7fc54d1b383
@kavinduushan07 <kavinduushan07@gmail.com>
2023-02-18 10:56:29 +0530

    create quiz component

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
 export default function Quiz() {
+
+  // Next button event handler
+  function onNext(){
+    console.log('on next click')
+  }
+  // Prev button event handler
+  function onPrev(){
+    console.log('on prev click')
+  }
+
   return (
-    <div>Quiz</div>
+    <div className='container'>
+      <h1 className='title text-light'>Quiz Application</h1>
+
+      {/* display questions */}
+
+      <div className='grid'>
+        <button className='btn prev' onClick={onPrev}>Prev</button>
+        <button className='btn next' onClick={onNext}>Next</button>
+      </div>
+    </div>
   )
 }
